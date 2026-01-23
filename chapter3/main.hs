19df1af54124e3b02c797e261b576125c1814d8e
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-01-23 22:23:27 +0900

    let case

--- chapter3/main.hs
+++ chapter3/main.hs
@@ -50,3 +50,15 @@ a `myCompare` b
 calcBmis :: [(Double, Double)] -> [Double]
 calcBmis xs = [bmi w h | (w, h) <- xs]
   where bmi weight height = weight / height ^ 2
+
+cylinder :: Double -> Double -> Double
+cylinder r h =
+  let sideArea = 2 * pi * r * h
+      topArea = pi * r ^ 2
+  in sideArea + 2 * topArea
+
+describeList :: [a] -> String
+describeList ls = "The list is" 
+                  ++ case ls of [] -> "empty."
+                                [x] -> "a singleton list."
+                                xs -> "a longer list."
