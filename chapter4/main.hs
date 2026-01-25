46574a3afa31db5ce0e20da87d106aefc5331106
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-01-25 10:35:01 +0900

    Add implementations for take', reverse', repeat', zip', and elm' functions

--- chapter4/main.hs
+++ chapter4/main.hs
@@ -7,3 +7,27 @@ replicate' :: Int -> a -> [a]
 replicate' n x
   | n <= 0 = []
   | otherwise = x : replicate' (n-1) x
+
+take' :: Int -> [a]-> [a]
+take' n _
+  | n <= 0 = []
+take' _ [] = []
+take' n (x:xs) = x : take' (n-1) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x : repeat' x
+
+zip' :: [a] -> [b] -> [(a, b)]
+zip' [] _ = []
+zip' _ [] = []
+zip' (x:xs) (y:ys) = (x, y) : zip' xs ys
+
+elm' :: (Eq a) => a -> [a] -> Bool
+elm' a [] = False
+elm' a (x:xs)
+  | a == x = True
+  | otherwise = a `elm'` xs
