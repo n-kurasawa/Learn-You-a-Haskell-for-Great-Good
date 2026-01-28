0984e1670b7247948c542c974413c734092e2c5e
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-01-28 23:25:51 +0900

    Add flip' function to enable argument reordering in function application

--- chapter5/main.hs
+++ chapter5/main.hs
@@ -6,6 +6,11 @@ zipWith' _ [] _ = []
 zipWith' _ _ [] = []
 zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
 
+flip' :: (a -> b -> c) -> (b -> a -> c)
+-- flip' f = g
+--   where g x y = f y x
+flip' f x y = f y x
+
 main :: IO ()
 main = do
   print (applyTwice (+3) 10)
