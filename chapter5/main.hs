c4e48381311c7862069a92021ca7e516b0e79263
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-01-31 08:38:49 +0900

    Refactor numLongChains to use a lambda function for clarity

--- chapter5/main.hs
+++ chapter5/main.hs
@@ -32,8 +32,7 @@ chain n
   | odd n = n : chain (n * 3 + 1)
 
 numLongChains :: Int
-numLongChains = length (filter isLong (map chain [1..100]))
-  where isLong xs = length xs > 15
+numLongChains = length (filter (\xs -> length xs > 15) (map chain [1..100]))
 
 main :: IO ()
 main = do
