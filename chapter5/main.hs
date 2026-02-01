d2156d3374a43a23fe8627ffab56af64bdf75054
n-kurasawa <kurasawa.nk.naohiro@gmail.com>
2026-02-01 15:27:12 +0900

    5.5

--- chapter5/main.hs
+++ chapter5/main.hs
@@ -6,6 +6,17 @@ zipWith' _ [] _ = []
 zipWith' _ _ [] = []
 zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
 
+main :: IO ()
+main = do
+  print (applyTwice (+3) 10)
+  print (applyTwice (++ " HAHA") "HEY")
+  print (applyTwice (3:) [1])
+  let result1 = zipWith' (+) [4,2,5,6] [2,6,2,3]
+      result2 = zipWith' max [6,3,2,1] [7,3,1,5]
+      in do
+        print result1
+        print result2
+
 flip' :: (a -> b -> c) -> (b -> a -> c)
 -- flip' f = g
 --   where g x y = f y x
@@ -40,13 +51,26 @@ sum' xs  = foldl (\acc x -> acc + x) 0 xs
 sum'' :: (Num a) => [a] -> a
 sum'' = foldl (+) 0
 
-main :: IO ()
-main = do
-  print (applyTwice (+3) 10)
-  print (applyTwice (++ " HAHA") "HEY")
-  print (applyTwice (3:) [1])
-  let result1 = zipWith' (+) [4,2,5,6] [2,6,2,3]
-      result2 = zipWith' max [6,3,2,1] [7,3,1,5]
-      in do
-        print result1
-        print result2
+map'' :: (a -> b) -> [a] ->  [b]
+map'' f xs = foldr (\x acc -> f x : acc) [] xs
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' y ys = foldr (\x acc -> if x == y then True else acc) False ys
+
+maximum' :: (Ord a) =>  [a] ->  a
+maximum' = foldl1 max
+
+reverse' :: [a] -> [a]
+reverse' = foldl (flip (:)) []
+
+product' :: (Num a) => [a] -> a
+product' = foldl (*) 1
+
+filter'' :: (a -> Bool) -> [a] -> [a]
+filter'' p = foldr (\x acc -> if p x then x : acc else acc) []
+
+last' :: [a] -> a
+last' = foldl1 (\_ x -> x)
+
+sqrtSums :: Int
+sqrtSums = length (takeWhile (<1000) (scanl1 (+) (map sqrt [1..]))) + 1
