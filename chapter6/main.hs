30404a13784f35d4e96db179eb247cfe9d3ac191
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-02-04 22:23:29 +0900

    Add wordNums function to count occurrences of words in a string

--- chapter6/main.hs
+++ chapter6/main.hs
@@ -1,4 +1,7 @@
-import Data.List (nub, sort)
+import Data.List (nub, sort, group)
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
+
+wordNums :: String -> [(String, Int)]
+wordNums = map (\ws -> (head ws, length ws)) . group . sort . words
