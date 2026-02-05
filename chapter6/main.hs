78fcb55f0bb6eaca1e04cb4eab0a6ccf9bd21162
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-02-05 23:14:53 +0900

    Add encoding functions and digit sum calculation

--- chapter6/main.hs
+++ chapter6/main.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Data.Char
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
@@ -8,3 +9,12 @@ wordNums = map (\ws -> (head ws, length ws)) . group . sort . words
 
 isIn :: (Eq a) => [a] -> [a] -> Bool
 needle `isIn` haystack = any (needle `isPrefixOf`) (tails haystack)
+
+encode :: Int -> String -> String
+encode offset msg = map (\c -> chr $ ord c + offset) msg
+
+digitSum :: Int -> Int
+digitSum = sum . map digitToInt . show
+
+firstTo40 :: Maybe Int
+firstTo40 = find (\x -> digitSum x == 40) [1..]
