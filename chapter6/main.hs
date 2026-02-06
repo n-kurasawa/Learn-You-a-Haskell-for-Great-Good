3bf2d0235664010354f6da4cf3029089627f3b14
n-kurasawa <kurasawa.nk.naohiro@gmail.com>
2026-02-06 21:36:06 +0900

    findkey

--- chapter6/main.hs
+++ chapter6/main.hs
@@ -18,3 +18,22 @@ digitSum = sum . map digitToInt . show
 
 firstTo40 :: Maybe Int
 firstTo40 = find (\x -> digitSum x == 40) [1..]
+
+phoneBook =
+    [("betty", "555-2938")   
+    ,("bonnie", "452-2928")
+    ,("patsy", "493-2928")   
+    ,("lucille", "205-2928")   
+    ,("wendy", "939-8282")
+    ,("penny", "853-2492")
+    ]
+
+findkey :: (Eq k) => k -> [(k,v)] -> Maybe v
+-- findkey key [] = Nothing
+-- findkey key ((k,v):xs)
+--     | key == k = Just v
+--     | otherwise = findkey key xs
+
+findkey key xs = foldr
+                (\(k,v) acc -> if key == k then Just v else acc)
+                Nothing xs
