c7f1ae2afbd456d7aedd45c7a5b3cd23b20f9fc2
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-02-12 23:17:12 +0900

    vector

--- chapter7/main.hs
+++ chapter7/main.hs
@@ -2,3 +2,11 @@ import Shapes
 
 main = do
   print $ nudge (baseCircle 10) 2 3
+
+data Vector a = Vector a a a deriving (Show)
+
+vplus :: (Num a) => Vector a -> Vector a -> Vector a
+(Vector i j k) `vplus` (Vector l m n) = Vector (i + l) (j + m) (k + n)
+
+vmult :: (Num a) => Vector a -> a -> Vector a
+(Vector i j k) `vmult` m = Vector (i * m) (j * m) (k * m)
