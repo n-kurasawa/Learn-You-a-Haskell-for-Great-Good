ea9fda8f474406cec60c7d5c1cbf980dda21524c
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-02-14 10:41:51 +0900

    add Tree

--- chapter7/main.hs
+++ chapter7/main.hs
@@ -32,3 +32,24 @@ lockerLookup lockerNumber map = case Map.lookup lockerNumber map of
     if state /= Taken
       then Right code
       else Left $ "Locker " ++ show lockerNumber ++ " is alrady taken!"
+
+data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord)
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+  | x == a = Node x left right
+  | x < a = Node a (treeInsert x left) right
+  | x > a = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+  | x == a = True
+  | x < a = treeElem x left
+  | x > a = treeElem x right
