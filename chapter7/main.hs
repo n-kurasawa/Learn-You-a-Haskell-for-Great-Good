aecb1504e30ccbab749bc1e0e8ee2cc498596994
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-02-16 22:19:33 +0900

    tree functor

--- chapter7/main.hs
+++ chapter7/main.hs
@@ -90,3 +90,7 @@ yesnoif yesnoVal yesResult noResult =
   if yesno yesnoVal
     then yesResult
     else noResult
+
+instance Functor Tree where
+  fmap f EmptyTree = EmptyTree
+  fmap f (Node x left right) = Node (f x) (fmap f left) (fmap f right)
