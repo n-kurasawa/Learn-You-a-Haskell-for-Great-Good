0f0acd6d263b42fc0015965a6aff9ae81cdba813
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-02-20 23:26:47 +0900

    color

---
+++ chapter8/color.hs
@@ -0,0 +1,12 @@
+import Control.Monad
+
+main = do
+  colors <- forM [1, 2, 3, 4] $ \a -> do
+    putStrLn $
+      "Which color do you assosiate with the number "
+        ++ show a
+        ++ "?"
+    color <- getLine
+    return color
+  putStrLn "The colors that you assosiate with 1, 2, 3 and 4 are: "
+  mapM putStrLn colors
