7fd23c958d0e2986c9b7465a29b72237c47bcfe6
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-02-25 23:55:28 +0900

    todo

---
+++ chapter9/todo.hs
@@ -0,0 +1,53 @@
+import Data.List
+import System.Directory
+import System.Environment
+import System.IO
+
+dispatch :: String -> [String] -> IO ()
+dispatch "add" = add
+dispatch "view" = view
+dispatch "remove" = remove
+
+add :: [String] -> IO ()
+add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")
+
+view :: [String] -> IO ()
+view [fileName] = do
+  contents <- readFile fileName
+  let todoTasks = lines contents
+      numberedTasks =
+        zipWith
+          (\n line -> show n ++ " - " ++ line)
+          [0 ..]
+          todoTasks
+  putStr $ unlines numberedTasks
+
+remove :: [String] -> IO ()
+remove [fileName, numberString] = do
+  contents <- readFile fileName
+  let todoTasks = lines contents
+      numberdTasks =
+        zipWith
+          (\n line -> show n ++ " - " ++ line)
+          [0 ..]
+          todoTasks
+  putStrLn "These are your TO-DO items:"
+  mapM_ putStrLn numberdTasks
+  let number = read numberString
+      newTodoItems = unlines $ delete (todoTasks !! number) todoTasks
+  bracketOnError
+    (openTempFile "." "temp")
+    ( \(tempName, tempHandle) -> do
+        hClose tempHandle
+        removeFile tempName
+    )
+    ( \(tempName, tempHandle) -> do
+        hPutStr tempHandle newTodoItems
+        hClose tempHandle
+        removeFile fileName
+        renameFile tempName fileName
+    )
+
+main = do
+  (command : argList) <- getArgs
+  dispatch command argList
