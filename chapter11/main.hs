fdbca5b29afcbcb059cc3731a1d8afcbfaaf6abc
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-03-03 08:58:24 +0900

    Functor CMayby

--- chapter11/main.hs
+++ chapter11/main.hs
@@ -4,3 +4,9 @@ import Data.List
 main = do
   line <- fmap (intersperse '-' . reverse . map toUpper) getLine
   putStrLn line
+
+data CMayby a = CNothing | CJust Int a deriving (Show)
+
+instance Functor CMayby where
+  fmap f CNothing = CNothing
+  fmap f (CJust counter x) = CJust (counter + 1) (f x)
