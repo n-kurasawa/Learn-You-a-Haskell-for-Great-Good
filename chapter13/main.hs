2740f5b16d3ae00d8eaf9466b932942122167495
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-03-12 23:01:36 +0900

    knight

--- chapter13/main.hs
+++ chapter13/main.hs
@@ -1,3 +1,5 @@
+import Control.Monad
+
 applyMaybe :: Maybe a -> (a -> Maybe b) -> Maybe b
 applyMaybe Nothing f = Nothing
 applyMaybe (Just x) f = f x
@@ -40,3 +42,29 @@ listOfTuples = do
   n <- [1, 2]
   ch <- ['a', 'b']
   return (n, ch)
+
+type KnightPos = (Int, Int)
+
+moveKnight :: KnightPos -> [KnightPos]
+moveKnight (c, r) = do
+  (c', r') <-
+    [ (c + 2, r - 1),
+      (c + 2, r + 1),
+      (c - 2, r - 1),
+      (c - 2, r + 1),
+      (c + 1, r - 2),
+      (c + 1, r + 2),
+      (c - 1, r - 2),
+      (c - 1, r + 2)
+      ]
+  guard (c' `elem` [1 .. 8] && r' `elem` [1 .. 8])
+  return (c', r')
+
+in3 :: KnightPos -> [KnightPos]
+in3 start = do
+  first <- moveKnight start
+  second <- moveKnight first
+  moveKnight second
+
+canReachIn3 :: KnightPos -> KnightPos -> Bool
+canReachIn3 start end = end `elem` in3 start
