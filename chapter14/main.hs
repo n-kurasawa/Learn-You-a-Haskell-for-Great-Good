59a6cc004ae32a05aaec1e256c1a2f4c66f1df44
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-03-18 21:55:20 +0900

    difflist

--- chapter14/main.hs
+++ chapter14/main.hs
@@ -48,3 +48,10 @@ instance Semigroup (DiffList a) where
 
 instance Monoid (DiffList a) where
   mempty = DiffList (\xs -> [] ++ xs)
+
+finalCountDown :: Int -> Writer (DiffList String) ()
+finalCountDown 0 = do
+  tell (toDiffList ["0"])
+finalCountDown x = do
+  finalCountDown (x - 1)
+  tell (toDiffList [show x])
