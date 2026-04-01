e6ed48c104aafad9344f96e90a4b724e1293a0a1
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-04-01 23:46:02 +0900

    flip

--- chapter14/monad.hs
+++ chapter14/monad.hs
@@ -1,9 +1,15 @@
+import Control.Monad (ap, liftM)
+import Data.List (all)
 import Data.Ratio
 
 newtype Prob a = Prob {getProb :: [(a, Rational)]} deriving (Show)
 
 instance Functor Prob where
   fmap f (Prob xs) = Prob $ map (\(x, p) -> (f x, p)) xs
+
+instance Applicative Prob where
+  pure x = Prob [(x, 1 % 1)]
+  (<*>) = ap
 
 thisSituation :: Prob (Prob Char)
 thisSituation =
@@ -20,4 +26,18 @@ flatten (Prob xs) = Prob $ concat $ map multAll xs
 instance Monad Prob where
   return x = Prob [(x, 1 % 1)]
   m >>= f = flatten (fmap f m)
-  fail _ = Prob []
+
+data Coin = Heads | Tails deriving (Show, Eq)
+
+coin :: Prob Coin
+coin = Prob [(Heads, 1 % 2), (Tails, 1 % 2)]
+
+loadedCoin :: Prob Coin
+loadedCoin = Prob [(Heads, 1 % 10), (Tails, 9 % 10)]
+
+flipThree :: Prob Bool
+flipThree = do
+  a <- coin
+  b <- coin
+  c <- loadedCoin
+  return (all (== Tails) [a, b, c])
