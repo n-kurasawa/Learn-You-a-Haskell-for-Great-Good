fc0f237b515fe1b69bb4b343667126bd5b420c2a
n-kurasawa <naohiro.kurasawa@kaminashi.com>
2026-04-03 14:00:31 +0900

    Breadcrumbs

--- chapter15/main.hs
+++ chapter15/main.hs
@@ -30,3 +30,30 @@ freeTree =
             (Node 'C' Empty Empty)
         )
     )
+
+data Direction = L | R deriving (Show)
+
+type Directions = [Direction]
+
+changeToP :: Directions -> Tree Char -> Tree Char
+changeToP (L : ds) (Node x l r) = Node x (changeToP ds l) r
+changeToP (R : ds) (Node x l r) = Node x l (changeToP ds r)
+changeToP [] (Node _ l r) = Node 'P' l r
+
+elemAt :: Directions -> Tree a -> a
+elemAt (L : ds) (Node _ l _) = elemAt ds l
+elemAt (R : ds) (Node _ _ r) = elemAt ds r
+elemAt [] (Node x _ _) = x
+
+type Breadcrumbs = [Direction]
+
+goLeft :: (Tree a, Breadcrumbs) -> (Tree a, Breadcrumbs)
+goLeft (Node _ l _, bs) = (l, L : bs)
+
+goRight :: (Tree a, Breadcrumbs) -> (Tree a, Breadcrumbs)
+goRight (Node _ _ r, bs) = (r, R : bs)
+
+data Crumb a
+  = LeftCrumb a (Tree a)
+  | RightCrumb a (Tree a)
+  deriving (Show)
